9d0166b0d82a3c1d0c6eeb67b8ed1a8c1b652048
Daniel <danieljgallina@gmail.com>
2023-05-09 03:55:49 -0600

    add scroll reveal

---
+++ js/scrollreveal.js
@@ -0,0 +1,197 @@
+// HOME
+ScrollReveal().reveal('.home-main',{
+    duration   : 350,
+    distance   : '20px',
+    easing     : 'ease-in',
+    origin     : 'left',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.txt-1',{
+    duration   : 300,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.title',{
+    duration   : 400,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.txt-2',{
+    duration   : 500,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.flipdown',{
+    duration   : 550,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.txt-3',{
+    duration   : 600,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.txt-4',{
+    duration   : 650,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.txt-5',{
+    duration   : 700,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+
+// OUR HISTORY
+ScrollReveal().reveal('.title-history',{
+    duration   : 300,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'top',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.our-history-img',{
+    duration   : 300,
+    distance   : '25px',
+    easing     : 'ease-in',
+    origin     : 'left',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.rectangle',{
+    duration   : 350,
+    distance   : '25px',
+    easing     : 'ease-in',
+    origin     : 'top',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.oh-txt-1',{
+    duration   : 450,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'top',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.oh-txt-2',{
+    duration   : 525,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'top',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.oh-txt-3',{
+    duration   : 600,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'top',
+    reset      : false,
+    scale      : 1,
+})
+
+
+// DETAILS
+ScrollReveal().reveal('.sub-title',{
+    duration   : 300,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'top',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.detail-content-text',{
+    duration   : 400,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.detail-img',{
+    duration   : 480,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+
+// UBICATION
+ScrollReveal().reveal('.u-iframe',{
+    duration   : 480,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.u-txt-1',{
+    duration   : 520,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+ScrollReveal().reveal('.u-txt-2',{
+    duration   : 600,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'bottom',
+    reset      : false,
+    scale      : 1,
+})
+
+
+// REGISTER
+ScrollReveal().reveal('.img-register',{
+    duration   : 400,
+    distance   : '30px',
+    easing     : 'ease-in',
+    origin     : 'left',
+    reset      : false,
+    scale      : 1,
+})
